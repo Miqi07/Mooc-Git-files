4f94686001c32dd36f9ec808f1c5416b3af93148
Miqi <bagonzaimran59@gmail.com>
2026-01-26 22:22:25 +0300

    Theadvanced level

--- mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,18 +3,44 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
         // part 1 of the exercise
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        int i = 1;
+        while (i<=size) {
+            printSpaces(size - i);
+            printStars(size);
+            i++;
+        }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        int i = 0;
+        while (height>= i) {
+            printStars(height - (height - i));
+            printSpaces(height-i);
+            i++;
+        }
+
+        for (int j =0; j <= 2; j++) {
+            printSpaces(height-3);
+            printStars(3);
+        }
+       
+
     }
 
     public static void main(String[] args) {
